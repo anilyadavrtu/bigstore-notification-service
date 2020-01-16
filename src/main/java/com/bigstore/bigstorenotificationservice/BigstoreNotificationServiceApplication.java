6fa8c37e93196586e170f4bbde04be85b9bf1ce6
anilyadavrtu <anilyadav.rtu@gmail.com>
2020-01-16 19:38:37 +0530

    base project setup

---
+++ src/main/java/com/bigstore/bigstorenotificationservice/BigstoreNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bigstore.bigstorenotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigstoreNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigstoreNotificationServiceApplication.class, args);
+	}
+
+}
